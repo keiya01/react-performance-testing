cfe0ccc9ba67697603a9439e8d3766808d232a5e
keiya01 <keiya.s.0210@gmail.com>
2020-08-09 11:19:15 +0900

    added test about memo component

--- src/__tests__/classComponent.test.tsx
+++ src/__tests__/classComponent.test.tsx
@@ -64,3 +64,74 @@ test('should get 2 from renderCount.current.Text when state is updated', () => {
   expect(screen.queryByText('2')).toBeDefined();
   expect(renderCount.current.Text).toBe(2);
 });
+
+test('should get 1 from renderCount.current.* in initial render with memo()', () => {
+  class Text extends React.Component {
+    render() {
+      return <p>Test</p>;
+    }
+  }
+
+  class MemorizedComponent extends React.PureComponent {
+    render() {
+      return <Text />;
+    }
+  }
+
+  const { renderCount } = perf(React);
+
+  render(<MemorizedComponent />);
+
+  expect(renderCount.current.MemorizedComponent).toBe(1);
+  expect(renderCount.current.Text).toBe(1);
+});
+
+test('should get 2 from renderCount.current.Text when state is updated with memo()', () => {
+  class MemorizedText extends React.PureComponent {
+    render() {
+      return <p>memo</p>;
+    }
+  }
+
+  class Text extends React.Component<any, { count: number }> {
+    constructor(props: any) {
+      super(props);
+
+      this.state = {
+        count: 0,
+      };
+    }
+
+    updateCount = () => {
+      this.setState((prev) => ({ ...prev, count: prev.count + 1 }));
+    };
+
+    render() {
+      return (
+        <div>
+          <p>{this.state.count}</p>
+          <MemorizedText />
+          <button type="button" onClick={this.updateCount}>
+            count
+          </button>
+        </div>
+      );
+    }
+  }
+
+  class Component extends React.Component {
+    render() {
+      return <Text />;
+    }
+  }
+
+  const { renderCount } = perf(React);
+
+  render(<Component />);
+
+  fireEvent.click(screen.getByRole('button', { name: /count/i }));
+
+  expect(screen.queryByText('2')).toBeDefined();
+  expect(renderCount.current.Text).toBe(2);
+  expect(renderCount.current.MemorizedText).toBe(1);
+});
--- src/__tests__/functionComponent.test.tsx
+++ src/__tests__/functionComponent.test.tsx
@@ -41,3 +41,48 @@ test('should get 2 from renderCount.current.Text when state is updated', () => {
   expect(screen.queryByText('2')).toBeDefined();
   expect(renderCount.current.Text).toBe(2);
 });
+
+test('should get 1 from renderCount.current.* in initial render with memo()', () => {
+  const Text = () => <p>Test</p>;
+  const Component = React.memo(function MemorizedComponent() {
+    return <Text />;
+  });
+
+  const { renderCount } = perf(React);
+
+  render(<Component />);
+
+  expect(renderCount.current.MemorizedComponent).toBe(1);
+  expect(renderCount.current.Text).toBe(1);
+});
+
+test('should get 2 from renderCount.current.Text when state is updated with memo()', () => {
+  const Wrapper = React.memo(function MemorizedText() {
+    return <p>memo</p>;
+  });
+  const Text = () => {
+    const [count, setCount] = React.useState(0);
+    return (
+      <div>
+        <p>{count}</p>
+        <Wrapper />
+        <button type="button" onClick={() => setCount((c) => c + 1)}>
+          count
+        </button>
+      </div>
+    );
+  };
+  const Component = () => {
+    return <Text />;
+  };
+
+  const { renderCount } = perf(React);
+
+  render(<Component />);
+
+  fireEvent.click(screen.getByRole('button', { name: /count/i }));
+
+  expect(screen.queryByText('2')).toBeDefined();
+  expect(renderCount.current.Text).toBe(2);
+  expect(renderCount.current.MemorizedText).toBe(1);
+});
