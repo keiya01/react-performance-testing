ddc9e5082b633e467e44b2f5bc0255a8deb309c6
keiya01 <keiya.s.0210@gmail.com>
2020-08-09 11:18:51 +0900

    refactor perf method

--- src/perf.ts
+++ src/perf.ts
@@ -1,12 +1,6 @@
 import { PerfTools } from './types';
 import { getPatchedComponent } from './getPatchedComponent';
-import { getDisplayName } from './getDisplayName';
-import { isClassComponent } from './isClassComponent';
-
-const shouldObserve = (
-  component: any,
-): component is React.ComponentClass | React.FunctionComponent =>
-  isClassComponent(component) || typeof component === 'function';
+import { shouldTrack } from './utils/shouldTrack';
 
 export const perf = (React: any) => {
   const renderCount: { current: Record<string, any> } = { current: {} };
@@ -15,18 +9,27 @@ export const perf = (React: any) => {
   const origCreateFactory = React.createFactory;
   const origCloneElement = React.cloneElement;
 
+  // memorize MemoComponent
+  const componentsMap = new WeakMap();
+
   // @ts-ignore
   React.createElement = (type: React.ElementType, ...rest: any) => {
-    if (!shouldObserve(type)) {
+    if (!shouldTrack(type)) {
       return origCreateElement.apply(React, [type, ...rest]);
     }
 
-    const PatchedComponent = getPatchedComponent(type, { renderCount });
+    let PatchedComponent: any;
 
-    try {
-      // @ts-ignore
-      PatchedComponent.displayName = getDisplayName(type);
-    } catch (e) {}
+    if (componentsMap.has(type)) {
+      PatchedComponent = componentsMap.get(type);
+    } else {
+      PatchedComponent = getPatchedComponent(
+        componentsMap,
+        type,
+        { renderCount },
+        React,
+      );
+    }
 
     return origCreateElement.apply(React, [PatchedComponent, ...rest]);
   };
