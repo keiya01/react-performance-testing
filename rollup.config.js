8bbb94cd3199a3e4632b829bf8bee4fef1e7306f
keiya01 <keiya.s.0210@gmail.com>
2020-08-11 13:40:17 +0900

    added rollup

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+    },
+  ],
+  plugins: [typescript({ tsconfig: 'tsconfig.json' })],
+};
