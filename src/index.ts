052794e71511cc774c63c271a64a31ab5a35aaec
keiya01 <keiya.s.0210@gmail.com>
2020-08-11 17:43:54 +0900

    added comment

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { cleanup } from './perf';
 
 // Auto cleanup with Jest, Mocha, Jasmine or some lib that is supporting teardown().
 if (!process.env.RPT_SKIP_AUTO_CLEANUP) {
+  /* istanbul ignore else */
   if (typeof afterEach === 'function') {
     afterEach(cleanup);
     // @ts-ignore
