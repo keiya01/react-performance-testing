5a7f39d65f8478b45a84b7ce56af8b78a52c8db8
keiya01 <keiya.s.0210@gmail.com>
2020-08-11 22:28:25 +0900

    fixed condition

--- src/utils/symbols.ts
+++ src/utils/symbols.ts
@@ -5,12 +5,7 @@
 
 export type ReactSymbol = Symbol | number;
 
-export let REACT_MEMO_TYPE: ReactSymbol = 0xead3;
-export let REACT_FORWARD_REF_TYPE: ReactSymbol = 0xead0;
-
-const symbolFor = typeof Symbol === 'function' && Symbol.for;
-
-if (symbolFor) {
-  REACT_MEMO_TYPE = symbolFor('react.memo');
-  REACT_FORWARD_REF_TYPE = symbolFor('react.forward_ref');
-}
+export const REACT_MEMO_TYPE: ReactSymbol = Symbol.for('react.memo');
+export const REACT_FORWARD_REF_TYPE: ReactSymbol = Symbol.for(
+  'react.forward_ref',
+);
