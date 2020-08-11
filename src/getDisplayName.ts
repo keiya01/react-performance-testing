5f40ede19ccac78344922188cb2a65aa69ebbc97
keiya01 <keiya.s.0210@gmail.com>
2020-08-11 18:14:48 +0900

    fixed getDisplayName

--- src/getDisplayName.ts
+++ src/getDisplayName.ts
@@ -2,5 +2,4 @@ export const getDisplayName = (type: any): any =>
   type.displayName ||
   type.name ||
   (type.type && getDisplayName(type.type)) ||
-  (type.render && getDisplayName(type.render)) ||
-  (typeof type === 'string' ? type : undefined);
+  (type.render && getDisplayName(type.render));
