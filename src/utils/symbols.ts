fb4526cad7b6673907aa7ad1bccf741dbd1debd0
keiya01 <keiya.s.0210@gmail.com>
2020-08-12 15:54:33 +0900

    revert symbols change

--- src/utils/symbols.ts
+++ src/utils/symbols.ts
@@ -5,7 +5,13 @@
 
 export type ReactSymbol = Symbol | number;
 
-export const REACT_MEMO_TYPE: ReactSymbol = Symbol.for('react.memo');
-export const REACT_FORWARD_REF_TYPE: ReactSymbol = Symbol.for(
-  'react.forward_ref',
-);
+export let REACT_MEMO_TYPE: ReactSymbol = 0xead3;
+export let REACT_FORWARD_REF_TYPE: ReactSymbol = 0xead0;
+
+const symbolFor = typeof Symbol === 'function' && Symbol.for;
+
+/* istanbul ignore else */
+if (symbolFor) {
+  REACT_MEMO_TYPE = Symbol.for('react.memo');
+  REACT_FORWARD_REF_TYPE = Symbol.for('react.forward_ref');
+}
