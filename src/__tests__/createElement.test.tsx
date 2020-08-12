701e8be277fd0c45c4b74ad43feaa21764d7924d
keiya01 <keiya.s.0210@gmail.com>
2020-08-12 16:11:37 +0900

    refactor createElement test

--- src/__tests__/createElement.test.tsx
+++ src/__tests__/createElement.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { perf } from '../index';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 describe('FunctionComponent', () => {
-  it('should get 1 from renderCount.current.* in initial render with flat structure', () => {
+  it('should initialize Component with flat structure', () => {
     const Text = () => <p>test</p>;
     const Component = () => {
       return <Text />;
@@ -19,7 +19,7 @@ describe('FunctionComponent', () => {
     });
   });
 
-  it('should get 1 from renderCount.current.* in initial render with nested structure', () => {
+  it('should initialize Component with nested structure', () => {
     const NestedText = () => <p>test</p>;
     const Text = () => <NestedText />;
     const Component = () => {
@@ -44,78 +44,7 @@ describe('FunctionComponent', () => {
     });
   });
 
-  it('should get 2 from renderCount.current.Text when state is updated with flat structure', () => {
-    const Text = () => {
-      const [count, setCount] = React.useState(0);
-      return (
-        <div>
-          <p>{count}</p>
-          <button type="button" onClick={() => setCount((c) => c + 1)}>
-            count
-          </button>
-        </div>
-      );
-    };
-    const Component = () => {
-      return <Text />;
-    };
-
-    const { renderCount } = perf(React);
-
-    render(<Component />);
-
-    fireEvent.click(screen.getByRole('button', { name: /count/i }));
-
-    expect(screen.queryByText('2')).toBeDefined();
-    expect(renderCount.current).toEqual({
-      Text: { value: 2 },
-      Component: { value: 1 },
-    });
-  });
-
-  it('should get 2 from renderCount.current.Text when state is updated with nested structure', () => {
-    const NestedText = ({ count }: { count: number }) => <p>{count}</p>;
-    const Text = ({ testid }: { testid?: string }) => {
-      const [count, setCount] = React.useState(0);
-      return (
-        <div>
-          <NestedText count={count} />
-          <button
-            data-testid={testid}
-            type="button"
-            onClick={() => setCount((c) => c + 1)}
-          >
-            count
-          </button>
-        </div>
-      );
-    };
-    const Component = () => {
-      return (
-        <>
-          <Text testid="button1" />
-          <Text testid="button2" />
-          <Text />
-        </>
-      );
-    };
-
-    const { renderCount } = perf(React);
-
-    render(<Component />);
-
-    fireEvent.click(screen.getByTestId('button1'));
-    fireEvent.click(screen.getByTestId('button2'));
-
-    expect(screen.queryByText('2')).toBeDefined();
-    expect(renderCount.current).toEqual({
-      NestedText: [{ value: 2 }, { value: 2 }, { value: 1 }],
-      Text: [{ value: 2 }, { value: 2 }, { value: 1 }],
-      Component: { value: 1 },
-    });
-  });
-
-  it('should get 1 from renderCount.current.* in initial render with memo()', () => {
+  it('should initialize Component with memo()', () => {
     const Text = () => <p>Test</p>;
     const Component = React.memo(function MemorizedComponent() {
       return <Text />;
@@ -128,40 +57,6 @@ describe('FunctionComponent', () => {
     expect(renderCount.current).toEqual({
       MemorizedComponent: { value: 1 },
       Text: { value: 1 },
-    });
-  });
-
-  it('should get 2 from renderCount.current.Text when state is updated with memo()', () => {
-    const Wrapper = React.memo(function MemorizedText() {
-      return <p>memo</p>;
-    });
-    const Text = () => {
-      const [count, setCount] = React.useState(0);
-      return (
-        <div>
-          <p>{count}</p>
-          <Wrapper />
-          <button type="button" onClick={() => setCount((c) => c + 1)}>
-            count
-          </button>
-        </div>
-      );
-    };
-    const Component = () => {
-      return <Text />;
-    };
-
-    const { renderCount } = perf(React);
-
-    render(<Component />);
-
-    fireEvent.click(screen.getByRole('button', { name: /count/i }));
-
-    expect(screen.queryByText('2')).toBeDefined();
-    expect(renderCount.current).toEqual({
-      MemorizedText: { value: 1 },
-      Text: { value: 2 },
-      Component: { value: 1 },
     });
   });
 
@@ -200,7 +95,7 @@ describe('FunctionComponent', () => {
 });
 
 describe('ClassComponent', () => {
-  it('should get 1 from renderCount.current.* in initial render with flat structure', () => {
+  it('should initialize Component with flat structure', () => {
     class Text extends React.Component {
       render() {
         return <p>Test</p>;
@@ -223,7 +118,7 @@ describe('ClassComponent', () => {
     });
   });
 
-  it('should get 1 from renderCount.current.* in initial render with nested structure', () => {
+  it('should initial Component with nested structure', () => {
     class NestedText extends React.Component {
       render() {
         return <p>Test</p>;
@@ -260,126 +155,7 @@ describe('ClassComponent', () => {
     });
   });
 
-  it('should get 2 from renderCount.current.Text when state is updated with flat structure', () => {
-    class Text extends React.Component<any, { count: number }> {
-      constructor(props: any) {
-        super(props);
-
-        this.state = {
-          count: 0,
-        };
-      }
-
-      updateCount = () => {
-        this.setState((prev) => ({ ...prev, count: prev.count + 1 }));
-      };
-
-      render() {
-        return (
-          <div>
-            <p>{this.state.count}</p>
-            <button type="button" onClick={this.updateCount}>
-              count
-            </button>
-          </div>
-        );
-      }
-    }
-
-    class Component extends React.Component {
-      render() {
-        return <Text />;
-      }
-    }
-
-    const { renderCount } = perf(React);
-
-    render(<Component />);
-
-    fireEvent.click(screen.getByRole('button', { name: /count/i }));
-
-    expect(screen.queryByText('2')).toBeDefined();
-
-    expect(renderCount.current).toEqual({
-      Text: { value: 2 },
-      Component: { value: 1 },
-    });
-  });
-
-  it('should get 2 from renderCount.current.Text when state is updated with nested structure', () => {
-    interface NestedTextProps {
-      count: number;
-    }
-    class NestedText extends React.Component<NestedTextProps> {
-      constructor(props: NestedTextProps) {
-        super(props);
-      }
-
-      render() {
-        return <p>{this.props.count}</p>;
-      }
-    }
-
-    interface TextProps {
-      testid?: string;
-    }
-    class Text extends React.Component<TextProps, { count: number }> {
-      constructor(props: TextProps) {
-        super(props);
-
-        this.state = {
-          count: 0,
-        };
-      }
-
-      updateCount = () => {
-        this.setState((prev) => ({ ...prev, count: prev.count + 1 }));
-      };
-
-      render() {
-        return (
-          <div>
-            <NestedText count={this.state.count} />
-            <button
-              data-testid={this.props.testid}
-              type="button"
-              onClick={this.updateCount}
-            >
-              count
-            </button>
-          </div>
-        );
-      }
-    }
-
-    class Component extends React.Component {
-      render() {
-        return (
-          <>
-            <Text testid="button1" />
-            <Text testid="button2" />
-            <Text />
-          </>
-        );
-      }
-    }
-
-    const { renderCount } = perf(React);
-
-    render(<Component />);
-
-    fireEvent.click(screen.getByTestId('button1'));
-    fireEvent.click(screen.getByTestId('button2'));
-
-    expect(screen.queryByText('2')).toBeDefined();
-    expect(renderCount.current).toEqual({
-      NestedText: [{ value: 2 }, { value: 2 }, { value: 1 }],
-      Text: [{ value: 2 }, { value: 2 }, { value: 1 }],
-      Component: { value: 1 },
-    });
-  });
-
-  it('should get 1 from renderCount.current.* in initial render with memo()', () => {
+  it('should initialize Component with memo()', () => {
     class Text extends React.Component {
       render() {
         return <p>Test</p>;
@@ -399,59 +175,6 @@ describe('ClassComponent', () => {
     expect(renderCount.current).toEqual({
       Text: { value: 1 },
       MemorizedComponent: { value: 1 },
-    });
-  });
-
-  it('should get 2 from renderCount.current.Text when state is updated with memo()', () => {
-    class MemorizedText extends React.PureComponent {
-      render() {
-        return <p>memo</p>;
-      }
-    }
-
-    class Text extends React.Component<any, { count: number }> {
-      constructor(props: any) {
-        super(props);
-
-        this.state = {
-          count: 0,
-        };
-      }
-
-      updateCount = () => {
-        this.setState((prev) => ({ ...prev, count: prev.count + 1 }));
-      };
-
-      render() {
-        return (
-          <div>
-            <p>{this.state.count}</p>
-            <MemorizedText />
-            <button type="button" onClick={this.updateCount}>
-              count
-            </button>
-          </div>
-        );
-      }
-    }
-
-    class Component extends React.Component {
-      render() {
-        return <Text />;
-      }
-    }
-
-    const { renderCount } = perf(React);
-
-    render(<Component />);
-
-    fireEvent.click(screen.getByRole('button', { name: /count/i }));
-
-    expect(screen.queryByText('2')).toBeDefined();
-    expect(renderCount.current).toEqual({
-      MemorizedText: { value: 1 },
-      Text: { value: 2 },
-      Component: { value: 1 },
     });
   });
 
@@ -517,7 +240,7 @@ describe('ClassComponent', () => {
     });
   });
 
-  it('should get 1 from renderCount.current.* in initial render when render method is arrow function', () => {
+  it('should initialize Component when render method is arrow function', () => {
     class Text extends React.Component {
       render() {
         return <p>Test</p>;
---
+++ src/__tests__/renderCount.test.tsx
@@ -0,0 +1,284 @@
+import React from 'react';
+import { perf } from '../index';
+import { render, fireEvent, screen } from '@testing-library/react';
+
+describe('FunctionComponent', () => {
+  it('should get 2 from renderCount.current.Text when state is updated with flat structure', () => {
+    const Text = () => {
+      const [count, setCount] = React.useState(0);
+      return (
+        <div>
+          <p>{count}</p>
+          <button type="button" onClick={() => setCount((c) => c + 1)}>
+            count
+          </button>
+        </div>
+      );
+    };
+    const Component = () => {
+      return <Text />;
+    };
+
+    const { renderCount } = perf(React);
+
+    render(<Component />);
+
+    fireEvent.click(screen.getByRole('button', { name: /count/i }));
+
+    expect(screen.queryByText('2')).toBeDefined();
+    expect(renderCount.current).toEqual({
+      Text: { value: 2 },
+      Component: { value: 1 },
+    });
+  });
+
+  it('should get 2 from renderCount.current.Text when state is updated with nested structure', () => {
+    const NestedText = ({ count }: { count: number }) => <p>{count}</p>;
+    const Text = ({ testid }: { testid?: string }) => {
+      const [count, setCount] = React.useState(0);
+      return (
+        <div>
+          <NestedText count={count} />
+          <button
+            data-testid={testid}
+            type="button"
+            onClick={() => setCount((c) => c + 1)}
+          >
+            count
+          </button>
+        </div>
+      );
+    };
+    const Component = () => {
+      return (
+        <>
+          <Text testid="button1" />
+          <Text testid="button2" />
+          <Text />
+        </>
+      );
+    };
+
+    const { renderCount } = perf(React);
+
+    render(<Component />);
+
+    fireEvent.click(screen.getByTestId('button1'));
+    fireEvent.click(screen.getByTestId('button2'));
+
+    expect(screen.queryByText('2')).toBeDefined();
+    expect(renderCount.current).toEqual({
+      NestedText: [{ value: 2 }, { value: 2 }, { value: 1 }],
+      Text: [{ value: 2 }, { value: 2 }, { value: 1 }],
+      Component: { value: 1 },
+    });
+  });
+
+  it('should get 2 from renderCount.current.Text when state is updated with memo()', () => {
+    const Wrapper = React.memo(function MemorizedText() {
+      return <p>memo</p>;
+    });
+    const Text = () => {
+      const [count, setCount] = React.useState(0);
+      return (
+        <div>
+          <p>{count}</p>
+          <Wrapper />
+          <button type="button" onClick={() => setCount((c) => c + 1)}>
+            count
+          </button>
+        </div>
+      );
+    };
+    const Component = () => {
+      return <Text />;
+    };
+
+    const { renderCount } = perf(React);
+
+    render(<Component />);
+
+    fireEvent.click(screen.getByRole('button', { name: /count/i }));
+
+    expect(screen.queryByText('2')).toBeDefined();
+    expect(renderCount.current).toEqual({
+      MemorizedText: { value: 1 },
+      Text: { value: 2 },
+      Component: { value: 1 },
+    });
+  });
+});
+
+describe('ClassComponent', () => {
+  it('should get 2 from renderCount.current.Text when state is updated with flat structure', () => {
+    class Text extends React.Component<any, { count: number }> {
+      constructor(props: any) {
+        super(props);
+
+        this.state = {
+          count: 0,
+        };
+      }
+
+      updateCount = () => {
+        this.setState((prev) => ({ ...prev, count: prev.count + 1 }));
+      };
+
+      render() {
+        return (
+          <div>
+            <p>{this.state.count}</p>
+            <button type="button" onClick={this.updateCount}>
+              count
+            </button>
+          </div>
+        );
+      }
+    }
+
+    class Component extends React.Component {
+      render() {
+        return <Text />;
+      }
+    }
+
+    const { renderCount } = perf(React);
+
+    render(<Component />);
+
+    fireEvent.click(screen.getByRole('button', { name: /count/i }));
+
+    expect(screen.queryByText('2')).toBeDefined();
+
+    expect(renderCount.current).toEqual({
+      Text: { value: 2 },
+      Component: { value: 1 },
+    });
+  });
+
+  it('should get 2 from renderCount.current.Text when state is updated with nested structure', () => {
+    interface NestedTextProps {
+      count: number;
+    }
+    class NestedText extends React.Component<NestedTextProps> {
+      constructor(props: NestedTextProps) {
+        super(props);
+      }
+
+      render() {
+        return <p>{this.props.count}</p>;
+      }
+    }
+
+    interface TextProps {
+      testid?: string;
+    }
+    class Text extends React.Component<TextProps, { count: number }> {
+      constructor(props: TextProps) {
+        super(props);
+
+        this.state = {
+          count: 0,
+        };
+      }
+
+      updateCount = () => {
+        this.setState((prev) => ({ ...prev, count: prev.count + 1 }));
+      };
+
+      render() {
+        return (
+          <div>
+            <NestedText count={this.state.count} />
+            <button
+              data-testid={this.props.testid}
+              type="button"
+              onClick={this.updateCount}
+            >
+              count
+            </button>
+          </div>
+        );
+      }
+    }
+
+    class Component extends React.Component {
+      render() {
+        return (
+          <>
+            <Text testid="button1" />
+            <Text testid="button2" />
+            <Text />
+          </>
+        );
+      }
+    }
+
+    const { renderCount } = perf(React);
+
+    render(<Component />);
+
+    fireEvent.click(screen.getByTestId('button1'));
+    fireEvent.click(screen.getByTestId('button2'));
+
+    expect(screen.queryByText('2')).toBeDefined();
+    expect(renderCount.current).toEqual({
+      NestedText: [{ value: 2 }, { value: 2 }, { value: 1 }],
+      Text: [{ value: 2 }, { value: 2 }, { value: 1 }],
+      Component: { value: 1 },
+    });
+  });
+
+  it('should get 2 from renderCount.current.Text when state is updated with memo()', () => {
+    class MemorizedText extends React.PureComponent {
+      render() {
+        return <p>memo</p>;
+      }
+    }
+
+    class Text extends React.Component<any, { count: number }> {
+      constructor(props: any) {
+        super(props);
+
+        this.state = {
+          count: 0,
+        };
+      }
+
+      updateCount = () => {
+        this.setState((prev) => ({ ...prev, count: prev.count + 1 }));
+      };
+
+      render() {
+        return (
+          <div>
+            <p>{this.state.count}</p>
+            <MemorizedText />
+            <button type="button" onClick={this.updateCount}>
+              count
+            </button>
+          </div>
+        );
+      }
+    }
+
+    class Component extends React.Component {
+      render() {
+        return <Text />;
+      }
+    }
+
+    const { renderCount } = perf(React);
+
+    render(<Component />);
+
+    fireEvent.click(screen.getByRole('button', { name: /count/i }));
+
+    expect(screen.queryByText('2')).toBeDefined();
+    expect(renderCount.current).toEqual({
+      MemorizedText: { value: 1 },
+      Text: { value: 2 },
+      Component: { value: 1 },
+    });
+  });
+});
