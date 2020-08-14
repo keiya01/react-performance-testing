94e5054c69e8e371680ce744145beafca53fb1c7
keiya sasaki <34934510+keiya01@users.noreply.github.com>
2020-08-14 17:20:32 +0900

    Update src/native/perf.ts

--- src/native/perf.ts
+++ src/native/perf.ts
@@ -5,7 +5,7 @@ import { isForwardRefComponent } from '../utils/isForwardRefComponent';
 
 export const perf = <T = DefaultPerfToolsField>(React: any) => {
   // Copied from https://github.com/styled-components/styled-components/blob/master/packages/styled-components/src/native/index.js#L24
-  // We don't need wrap these components.
+  // We don't need to wrap these components.
   const aliases = `ActivityIndicator ActivityIndicatorIOS AnimatedComponent AppContainer ART Button DatePickerIOS DrawerLayoutAndroid
     Image ImageBackground ImageEditor ImageStore KeyboardAvoidingView ListView MapView Modal NavigatorIOS
     Picker PickerIOS ProgressBarAndroid ProgressViewIOS PressabilityDebugView ScrollView SegmentedControlIOS Slider
