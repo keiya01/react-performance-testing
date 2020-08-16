ed15fe47af7af2b25f3f4481266843760ecab553
keiya01 <keiya.s.0210@gmail.com>
2020-08-16 12:10:20 +0900

    added jest-performance-testing config

---
+++ packages/jest-performance-testing/jest.config.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const common = require('../../scripts/jest/jest.common.config');
+
+module.exports = {
+  ...common,
+  displayName: {
+    name: 'Jest',
+    color: 'green',
+  },
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
+  testMatch: ['**/*.test.ts?(x)'],
+};
