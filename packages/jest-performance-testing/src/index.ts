b68f0dec2def8b5b44b77486a7002256576883b5
Kotaro Sugawara <kotarella1110@gmail.com>
2020-08-20 08:02:07 +0900

    :cop: fix type error caused with VSCode only (#11)

--- packages/jest-performance-testing/src/index.ts
+++ packages/jest-performance-testing/src/index.ts
@@ -1,1 +1,13 @@
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace jest {
+    interface Matchers<R> {
+      toBeUpdatedWithin(expected: number | number[]): R;
+      toBeMounted(): R;
+      toBeMountedWithin(expected?: number): R;
+      toBeRenderedTimes(expected?: number): R;
+    }
+  }
+}
+
 import './expectExtends';
