5c9960041e91678dcfe1a07f08962d48e7e65252
keiya01 <keiya.s.0210@gmail.com>
2020-08-20 20:50:55 +0900

    fixed jest-performance-testing test

--- packages/jest-performance-testing/src/__tests__/toBeMountedWithin.test.tsx
+++ packages/jest-performance-testing/src/__tests__/toBeMountedWithin.test.tsx
@@ -1,56 +1,66 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import { perf } from '../../../react-performance-testing/src/index';
 import '../index';
 
-test('should throw error when component is not mounted', () => {
+test('should throw error when component is not mounted', async () => {
   const { renderTime } = perf<{ Component: unknown }>(React);
 
-  expect(() =>
-    expect(renderTime.current.Component).toBeMountedWithin(),
-  ).toThrow(/Specified component could not be found/);
+  await waitFor(() =>
+    expect(() =>
+      expect(renderTime.current.Component).toBeMountedWithin(),
+    ).toThrow(/Specified component could not be found/),
+  );
 });
 
-test('should true when mount time is less than expected time', () => {
+test('should true when mount time is less than expected time', async () => {
   const Component = () => <div>test</div>;
 
   const { renderTime } = perf<{ Component: unknown }>(React);
 
   render(<Component />);
 
-  expect(renderTime.current.Component).toBeMountedWithin(16);
+  await waitFor(() =>
+    expect(renderTime.current.Component).toBeMountedWithin(16),
+  );
 });
 
-test('should throw error when mount time is greater than expected time', () => {
+test('should throw error when mount time is greater than expected time', async () => {
   const Component = () => <div>test</div>;
 
   const { renderTime } = perf<{ Component: unknown }>(React);
 
   render(<Component />);
 
-  expect(() =>
-    expect(renderTime.current.Component).toBeMountedWithin(0),
-  ).toThrow(/toBeMountedWithin/);
+  await waitFor(() =>
+    expect(() =>
+      expect(renderTime.current.Component).toBeMountedWithin(0),
+    ).toThrow(/toBeMountedWithin/),
+  );
 });
 
-test('should true else if mount time is greater than expected time when using `not` declaration', () => {
+test('should true else if mount time is greater than expected time when using `not` declaration', async () => {
   const Component = () => <div>test</div>;
 
   const { renderTime } = perf<{ Component: unknown }>(React);
 
   render(<Component />);
 
-  expect(renderTime.current.Component).not.toBeMountedWithin(0);
+  await waitFor(() =>
+    expect(renderTime.current.Component).not.toBeMountedWithin(0),
+  );
 });
 
-test('should throw error even if mount time is less than expected time when using `not` declaration', () => {
+test('should throw error even if mount time is less than expected time when using `not` declaration', async () => {
   const Component = () => <div>test</div>;
 
   const { renderTime } = perf<{ Component: unknown }>(React);
 
   render(<Component />);
 
-  expect(() =>
-    expect(renderTime.current.Component).not.toBeMountedWithin(16),
-  ).toThrow(/not/);
+  await waitFor(() =>
+    expect(() =>
+      expect(renderTime.current.Component).not.toBeMountedWithin(16),
+    ).toThrow(/not/),
+  );
 });
--- packages/jest-performance-testing/src/__tests__/toBeRenderedTimes.test.tsx
+++ packages/jest-performance-testing/src/__tests__/toBeRenderedTimes.test.tsx
@@ -1,56 +1,66 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import { perf } from '../../../react-performance-testing/src/index';
 import '../index';
 
-test('should throw error when component is not mounted', () => {
+test('should throw error when component is not mounted', async () => {
   const { renderCount } = perf<{ Component: unknown }>(React);
 
-  expect(() =>
-    expect(renderCount.current.Component).toBeRenderedTimes(),
-  ).toThrow(/Specified component could not be found/);
+  await waitFor(() =>
+    expect(() =>
+      expect(renderCount.current.Component).toBeRenderedTimes(),
+    ).toThrow(/Specified component could not be found/),
+  );
 });
 
-test('should true when expected value is equal', () => {
+test('should true when expected value is equal', async () => {
   const Component = () => <div />;
 
   const { renderCount } = perf(React);
 
   render(<Component />);
 
-  expect(renderCount.current.Component).toBeRenderedTimes(1);
+  await waitFor(() =>
+    expect(renderCount.current.Component).toBeRenderedTimes(1),
+  );
 });
 
-test('should throw error when expected value not is equal', () => {
+test('should throw error when expected value not is equal', async () => {
   const Component = () => <div />;
 
   const { renderCount } = perf(React);
 
   render(<Component />);
 
-  expect(() =>
-    expect(renderCount.current.Component).toBeRenderedTimes(2),
-  ).toThrow(/toBeRenderedTimes/);
+  await waitFor(() =>
+    expect(() =>
+      expect(renderCount.current.Component).toBeRenderedTimes(2),
+    ).toThrow(/toBeRenderedTimes/),
+  );
 });
 
-test('should true even if expected value is not equal when using `not` declaration', () => {
+test('should true even if expected value is not equal when using `not` declaration', async () => {
   const Component = () => <div />;
 
   const { renderCount } = perf(React);
 
   render(<Component />);
 
-  expect(renderCount.current.Component).not.toBeRenderedTimes(2);
+  await waitFor(() =>
+    expect(renderCount.current.Component).not.toBeRenderedTimes(2),
+  );
 });
 
-test('should throw error even if expected value is equal when using `not` declaration', () => {
+test('should throw error even if expected value is equal when using `not` declaration', async () => {
   const Component = () => <div />;
 
   const { renderCount } = perf(React);
 
   render(<Component />);
 
-  expect(() =>
-    expect(renderCount.current.Component).not.toBeRenderedTimes(1),
-  ).toThrow(/not/);
+  await waitFor(() =>
+    expect(() =>
+      expect(renderCount.current.Component).not.toBeRenderedTimes(1),
+    ).toThrow(/not/),
+  );
 });
