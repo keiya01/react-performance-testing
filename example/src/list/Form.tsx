21367ea2d8b694cec7532ebf3a43c0ccfe784a18
keiya01 <keiya.s.0210@gmail.com>
2020-09-01 15:56:19 +0900

    fixed FormData type in Form component

--- example/src/list/Form.tsx
+++ example/src/list/Form.tsx
@@ -8,10 +8,14 @@ interface Props {
   onSubmit: React.EventHandler<React.FormEvent<HTMLFormElement>>;
 }
 
-export const Form = ({ onSubmit }: Props): React.ReactElement => {
-  return (
-    <form onSubmit={onSubmit}>
-      <input name="message" />
-    </form>
-  );
-};
+export const Form = React.memo(
+  function Form({ onSubmit }: Props): React.ReactElement {
+    return (
+      <form onSubmit={onSubmit}>
+        <input name="message" />
+        <button type="submit">submit</button>
+      </form>
+    );
+  },
+  (prevProps, nextProps) => prevProps.onSubmit === nextProps.onSubmit,
+);
