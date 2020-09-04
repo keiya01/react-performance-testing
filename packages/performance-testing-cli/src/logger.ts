43224a5832f4824ad223e10ad7f3d03eedfda9a0
keiya01 <keiya.s.0210@gmail.com>
2020-09-04 09:40:59 +0900

    improved logger

--- packages/performance-testing-cli/src/logger.ts
+++ packages/performance-testing-cli/src/logger.ts
@@ -1,1 +1,4 @@
-export const logError = (msg: Error | string) => console.error(msg);
+import { red } from 'chalk';
+
+export const logError = (msg: Error | string) =>
+  console.error(red('Error: '), msg);
