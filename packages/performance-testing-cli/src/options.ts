7655ff70db09ef0692d824e672eb25f7fb4857af
keiya01 <keiya.s.0210@gmail.com>
2020-09-22 11:34:34 +0900

    added concurrent options

--- packages/performance-testing-cli/src/options.ts
+++ packages/performance-testing-cli/src/options.ts
@@ -1,4 +1,4 @@
-import { matchDefault } from './constants/matchDefault';
+import { matchDefault, concurrentDefault } from './constants/defaultOptions';
 
 export const options = {
   cmd: {
@@ -19,5 +19,11 @@ export const options = {
     requiresArg: true,
     type: 'string',
     default: matchDefault,
+  },
+  concurrent: {
+    describe: 'Run concurrency with specified number.',
+    requiresArg: true,
+    type: 'number',
+    default: concurrentDefault,
   },
 } as const;
